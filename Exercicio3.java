119d14291c1c49b18681011cb6fc9c2c13894469
Samuel Brayan <85649169+samuelnascimento2021@users.noreply.github.com>
2022-03-21 21:42:54 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -2,40 +2,28 @@ package Exercicios_Lista1;
 import java.util.Scanner;
 public class Exercicio3 {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	
+	Scanner entrada = new Scanner (System.in);
+	
+	String frase;
+	int cntVogais = 0;
+	
+	System.out.println("Insira sua frase: ");
+	frase = entrada.nextLine();
+	
+	frase = frase.toUpperCase();
 		
-		Scanner entrada = new Scanner (System.in);
-		
-		int cntVogais = 0;
-		String frase, vogais="";
-		
-		System.out.println("Insira sua frase");
-		frase = entrada.nextLine();
-		
-		frase = frase.toLowerCase();
-		
-		for(int i=0; i < frase.length(); i++)
-			{
-				char c = frase.charAt(i);
-				if(c == 'a' | c == 'e' | c=='i' | c == 'o' | c == 'u')
-				{
-					vogais = vogais + Character.toString(c);
-				}
-			}
-		
-		if (vogais.contains("a"))
-          		cntVogais++;
-      		if (vogais.contains("e"))
-          		cntVogais++;
-      		if (vogais.contains("i"))
-        		cntVogais++;
-     		if (vogais.contains("o"))
-       		  	cntVogais++;
-      		if (vogais.contains("u"))
-         	   	cntVogais++;
-    
-    
-    		System.out.println("Na frase \"" + frase + "\" temos: " + cntVogais + " vogal(is).");
-	}
+	if(frase.indexOf("A")!=-1) cntVogais++;
+	if(frase.indexOf("E")!=-1) cntVogais++;
+	if(frase.indexOf("I")!=-1) cntVogais++;
+	if(frase.indexOf("O")!=-1) cntVogais++;
+	if(frase.indexOf("U")!=-1) cntVogais++;
+	
+			
+	System.out.println("Na frase \"" + frase + "\" temos: " + cntVogais + " vogal(is).");
 
+}
+
+}
 }
